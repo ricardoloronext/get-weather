38e5a13275b458b821d5e6d54a8a1d3bcd984589
ricardo.loro <ricardo.loro.next@bbva.com>
2021-01-28 08:22:24 +0100

    refactor(js): removed api key from code

--- clima/clima.js
+++ clima/clima.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
-const key = 'c0526b6b5ea8db947aca886a8df57236';
 
-const getWeather = async (city) => {
+const getWeather = async (city, key) => {
   const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`)
   return response.data.main.temp;
 }
